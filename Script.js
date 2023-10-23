8b9bdaab9e9767639ce6ab8e370dd30f7994bf22
066_ShabrinaZahraAulia <117991094+shbri08@users.noreply.github.com>
2023-10-23 20:12:06 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,16 +1,39 @@
-function showLoading(button) {
-    var loading = button.nextElementSibling; // Ambil elemen loading setelah tombol
-    
-    loading.style.display = "inline-block"; // Tampilkan elemen loading
-    
-    // Sembunyikan tombol "Lihat Resep" selama proses loading
-    button.style.display = "none";
-    
-    setTimeout(function() {
-        loading.style.display = "none"; // Sembunyikan elemen loading
-        button.style.display = "inline-block"; // Tampilkan kembali tombol "Lihat Resep"
-        
-        // Pindahkan ke halaman resep
-        window.location.href = button.href;
-    }, 20000); // Waktu dalam milidetik (di sini, 20000 milidetik = 20 detik)
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const recipeSearchInput = document.getElementById("recipeSearch");
+    const searchResults = document.getElementById("searchResults");
+    const catCardList = document.querySelectorAll(".catCardList");
+
+    recipeSearchInput.addEventListener("input", function() {
+        const searchTerm = recipeSearchInput.value.toLowerCase();
+
+        searchResults.innerHTML = ""; // Kosongkan daftar hasil pencarian
+
+        if (searchTerm.length === 0) {
+            searchResults.style.display = "none"; // Sembunyikan daftar hasil jika tidak ada pencarian
+            return;
+        }
+
+        const results = [];
+       
+        document.getElementById("recipeSearch").style.width = "500px"
+        document.getElementById("recipeSearch").style.padding = "10px"
+
+        // Loop melalui setiap resep
+        catCardList.forEach(function(card) {
+            const h3 = card.querySelector("h3");
+            const recipeTitle = h3.textContent.toLowerCase();
+
+            if (recipeTitle.includes(searchTerm)) {
+                results.push(card.outerHTML);
+            }
+        });
+
+        if (results.length > 0) {
+            searchResults.innerHTML = results.join(""); // Tampilkan hasil pencarian
+            searchResults.style.display = "block";
+        } else {
+            searchResults.innerHTML = "Tidak ada hasil ditemukan."; // Tampilkan pesan jika tidak ada hasil
+            searchResults.style.display = "block";
+        }
+    });
+});
